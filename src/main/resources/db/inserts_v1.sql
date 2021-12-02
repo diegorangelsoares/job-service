b3e3d71cf5401c102362c653261aaadbf550d968
Diego Rangel <diego.freire@dock.tech>
2021-12-02 16:17:50 -0300

    Ajustes de scripts

--- src/main/resources/db/inserts_v1.sql
+++ src/main/resources/db/inserts_v1.sql
@@ -1,13 +1,13 @@
 INSERT INTO t_emissor (ID,CAMINHO, NOME, usuario, SENHA, servico)
-VALUES ((select max(id) from t_emissor)+1,'172.30.0.250','emissor1', 'asd', '123','dbdados','S');
+VALUES ((select max(id) from t_emissor)+1,'0.0.0.0','emissor1', 'asd', '123','dbdados','S');
 
 
 INSERT INTO t_emissor (ID,CAMINHO, NOME, usuario, SENHA, servico)
-VALUES ((select max(id) from t_emissor)+1,'172.30.0.222','emissor2', 'asd', '123','dbdados','S');
+VALUES ((select max(id) from t_emissor)+1,'0.0.0.0','emissor2', 'asd', '123','dbdados','S');
 
 
 INSERT INTO t_emissor (ID,CAMINHO, NOME, usuario, SENHA, servico)
-VALUES ((select max(id) from t_emissor)+1,'10.0.11.38','emissor3', 'asd', '123','dbdados','S');
+VALUES ((select max(id) from t_emissor)+1,'0.0.0.0','emissor3', 'asd', '123','dbdados','S');
 
 
 
