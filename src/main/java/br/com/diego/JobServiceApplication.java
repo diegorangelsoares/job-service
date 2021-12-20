0de4ccf20ae5cddf08ba9678956c296fefff45ad
Diego Rangel <diego.freire@dock.tech>
2021-12-20 09:25:23 -0300

    Ajustes em novas funcionalidades

--- src/main/java/br/com/diego/JobServiceApplication.java
+++ src/main/java/br/com/diego/JobServiceApplication.java
@@ -9,7 +9,7 @@ public class JobServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JobServiceApplication.class, args);
 		System.out.println("Link do swagger: http://localhost:8888/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#/");
-		System.out.println("Link do swagger: http://172.21.0.3/:8888/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#/");
+		System.out.println("Link do swagger: http://172.21.0.3:8888/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#/");
 	}
 
 }
